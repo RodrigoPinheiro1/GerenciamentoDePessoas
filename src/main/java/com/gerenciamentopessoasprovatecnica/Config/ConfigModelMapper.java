4d2d80414989cd06f6289d13b235feae66bfbf6a
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2023-01-03 18:27:37 -0300

    ConfigModelMapper

---
+++ src/main/java/com/gerenciamentopessoasprovatecnica/Config/ConfigModelMapper.java
@@ -0,0 +1,14 @@
+package com.gerenciamentopessoasprovatecnica.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigModelMapper {
+
+    @Bean
+    public ModelMapper modelMapper (){
+        return new ModelMapper();
+    }
+}
