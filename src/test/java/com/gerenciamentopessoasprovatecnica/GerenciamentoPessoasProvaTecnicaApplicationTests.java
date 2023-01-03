1bb5ed82377c4eb986da9b3a000cfc1f1ae4ce74
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2023-01-03 19:58:31 -0300

    adicionado testes para crud

---
+++ src/test/java/com/gerenciamentopessoasprovatecnica/GerenciamentoPessoasProvaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentopessoasprovatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPessoasProvaTecnicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
